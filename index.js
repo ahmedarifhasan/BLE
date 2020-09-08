9b32d3ef1b369f71e453f8528226fb5a64d713e9
ahmedarifhasan <roxer.arif@gmail.com>
2020-09-08 13:52:19 +0530

    Initial com=m

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const app = require('express')();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser') 
 
+
+
+app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.urlencoded({extended:true}));
 
