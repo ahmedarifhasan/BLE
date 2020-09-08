186e189c8432399bf0b1afd35c852c384bfaa062
ahmedarifhasan <37695517+ahmedarifhasan@users.noreply.github.com>
2020-09-08 13:56:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.post('/',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("Listening in port 3000");
-})
+// app.listen(3000,()=>{
+//     console.log("Listening in port 3000");
+// })
