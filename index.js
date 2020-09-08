37694b20cc6287eac2924ae4f070840a049183c0
ahmedarifhasan <37695517+ahmedarifhasan@users.noreply.github.com>
2020-09-08 13:56:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 
 
-app.use(express.static(__dirname + '/public'))
+// app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.urlencoded({extended:true}));
 
